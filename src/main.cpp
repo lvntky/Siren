11d7ed4411659c1b4bd58104b2894dbdcd093ada
lvntky <klevent1903@gmail.com>
2024-04-19 12:04:33 +0300

    [feature] basic bresenham line drawing

--- src/main.cpp
+++ src/main.cpp
@@ -9,13 +9,18 @@ int main()
     tga_image_t *image = tga_new(1024, 1024);
     tga_set_bg(image, black);
 
-    BresenhamManager bresenhamManager = BresenhamManager(0, 0, 512, 512, image, white);
+    BresenhamManager bresenhamManager = BresenhamManager(0, 1023, 1023, 0, image, white);
     bresenhamManager.drawLine();
-    bresenhamManager.setEndpoints(1023, 0, 512, 512);
+    bresenhamManager.setEndpoints(0, 0,1023, 0);
+    bresenhamManager.setColor(white);
+    bresenhamManager.drawLine();
+
+    bresenhamManager.setEndpoints(1023, 1023,0, 1023);
     bresenhamManager.setColor(white);
     bresenhamManager.drawLine();
 
     tga_write("bresenham.tga", image);
+    tga_free(image);
     
     return 0;
 }
