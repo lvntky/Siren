2cc0f29b0f9ac1cbcd0e6540062c67533058c6af
lvntky <klevent1903@gmail.com>
2024-04-19 12:37:34 +0300

    [feature] initial model parser strucure

--- cmake/SourcesAndHeaders.cmake
+++ cmake/SourcesAndHeaders.cmake
@@ -1,6 +1,7 @@
 set(sources
     src/targalib.cpp
     src/bresenham_manager.cpp
+    src/model_parser.cpp
 )
 
 set(exe_sources
@@ -11,7 +12,8 @@ set(exe_sources
 set(headers
     include/targalib/targalib.h
     include/siren/common.hpp
-    
+    include/siren/bresenham_manager.hpp
+    include/siren/model_parser.hpp
 )
 
 set(test_sources
--- include/siren/model_parser.hpp
+++ include/siren/model_parser.hpp
@@ -1,4 +1,33 @@
 #ifndef MODEL_PARSER_H_
 #define MODEL_PARSER_H_
 
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <vector>
+#include <string>
+
+struct Vec3
+{
+    float x, y, z;
+};
+
+struct FaceVertex
+{
+    int vertexIndex;
+    int normalIndex;
+};
+
+class ModelParser
+{
+private:
+    std::vector<Vec3> vertices;
+    std::vector<Vec3> normals;
+    std::vector<FaceVertex> faces;
+public:
+    ModelParser(const std::string& filename);
+    void printModelInfo();
+    ~ModelParser();
+};
+
 #endif //MODEL_PARSER_H_
